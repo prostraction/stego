a0787d1115393c713d4a1b78f781c01246b8491a
prostraction <47314760+prostraction@users.noreply.github.com>
2023-04-09 22:42:56 +0300

    started embedding

---
+++ src/embedding/embedding.go
@@ -0,0 +1,59 @@
+package embedding
+
+import (
+	"image"
+	_ "image"
+	"stego/src/dct"
+)
+
+func abs[T int | byte](value T) T {
+	if value < 0 {
+		return -value
+	}
+	return value
+}
+
+func Encode(img *image.RGBA, encodedWord string, pass string, addMod int, negMod int, channels int, channelSelected int) bool {
+	bounds := (*img).Bounds()
+	if bounds.Max.X < 8 || bounds.Max.Y < 8 {
+		return false
+	}
+	if len(encodedWord) == 0 {
+		return false
+	}
+	if len(pass) == 0 {
+		return false
+	}
+	const countValidIndexes = 25
+	const sizeOfBlock2D = 64
+	const sizeOfBlock1D = 8
+	validIndexes := [countValidIndexes]int{7, 14, 15, 21, 22, 23, 28, 29, 30, 31, 35, 36, 37, 38, 42, 43, 44, 45, 48, 49, 50, 51, 55, 56, 57}
+
+	currentSymbol := 0
+	dctMatrix := make([]float32, sizeOfBlock2D)
+	idctMatrix := make([]float32, sizeOfBlock2D)
+
+	for x := 0; x < bounds.Max.X-bounds.Max.X%sizeOfBlock1D-1; x += sizeOfBlock1D {
+		for y := 0; y < bounds.Max.Y-bounds.Max.Y%sizeOfBlock1D-1; y += sizeOfBlock1D {
+			pixelIndex1 := validIndexes[pass[y%len(pass)]%countValidIndexes]
+			pixelIndex2 := validIndexes[pass[x%len(pass)]%countValidIndexes]
+			pixelIndex3 := validIndexes[abs(pass[y%len(pass)]%countValidIndexes-pass[x%len(pass)]%countValidIndexes)%countValidIndexes]
+
+			for pixelIndex1 == pixelIndex2 {
+				pixelIndex2++
+				pixelIndex2 = validIndexes[pixelIndex2%countValidIndexes]
+			}
+			for (pixelIndex2 == pixelIndex3) || (pixelIndex1 == pixelIndex3) {
+				pixelIndex3++
+				pixelIndex3 = validIndexes[pixelIndex3%countValidIndexes]
+			}
+
+			dct.MakeDCT(&dctMatrix, &(*img).Pix, x, y, bounds.Max.X, 4, 0)
+
+			currentSymbol++
+		}
+
+	}
+
+	return true
+}
