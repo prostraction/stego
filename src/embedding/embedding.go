740e8b8b01a5e045163711863e855c456a025441
prostraction <47314760+prostraction@users.noreply.github.com>
2023-04-10 11:37:47 +0300

    encode and decode

--- src/embedding/embedding.go
+++ src/embedding/embedding.go
@@ -29,9 +29,7 @@ func stringToBoolArray(value string) []bool {
 			}
 			ch /= 2
 		}
-		//fmt.Println(len(symbol))
 		for j := 0; j < 8-len(symbol); j++ {
-			//fmt.Println(j)
 			array = append(array, false)
 		}
 		for j := len(symbol) - 1; j >= 0; j-- {
@@ -42,7 +40,7 @@ func stringToBoolArray(value string) []bool {
 	return array
 }
 
-func Encode(img *image.RGBA, encodedWord string, pass string, addMod int, negMod int, channelSelected int) bool {
+func Encode(img *image.RGBA, encodedWord string, pass string, encodedWordLen int, addMod int, negMod int, channelSelected int) bool {
 	bounds := (*img).Bounds()
 	if bounds.Max.X < 8 || bounds.Max.Y < 8 {
 		return false
@@ -60,8 +58,12 @@ func Encode(img *image.RGBA, encodedWord string, pass string, addMod int, negMod
 
 	currentSymbol := 0
 	dctMatrix := make([]float32, sizeOfBlock2D)
-	idctMatrix := make([]float32, sizeOfBlock2D)
 
+	boolPass := stringToBoolArray(pass)
+	if len(boolPass) < encodedWordLen {
+		emptySlice := make([]bool, encodedWordLen-len(pass))
+		boolPass = append(boolPass, emptySlice...)
+	}
 	for x := 0; x < bounds.Max.X-bounds.Max.X%sizeOfBlock1D-1; x += sizeOfBlock1D {
 		for y := 0; y < bounds.Max.Y-bounds.Max.Y%sizeOfBlock1D-1; y += sizeOfBlock1D {
 			pixelIndex1 := validIndexes[pass[y%len(pass)]%countValidIndexes]
@@ -77,11 +79,72 @@ func Encode(img *image.RGBA, encodedWord string, pass string, addMod int, negMod
 				pixelIndex3 = validIndexes[pixelIndex3%countValidIndexes]
 			}
 
-			dct.MakeDCT(&dctMatrix, &(*img).Pix, x, y, bounds.Max.X, 4, 0)
-
+			dct.MakeDCT(&dctMatrix, &(*img).Pix, x, y, bounds.Max.X, 4, int8(channelSelected))
+			if boolPass[currentSymbol%encodedWordLen] {
+				dctMatrix[pixelIndex1] = float32(addMod)
+				dctMatrix[pixelIndex2] = float32(addMod)
+				dctMatrix[pixelIndex3] = float32(negMod)
+			} else {
+				dctMatrix[pixelIndex1] = float32(negMod)
+				dctMatrix[pixelIndex2] = float32(negMod)
+				dctMatrix[pixelIndex3] = float32(addMod)
+			}
+			dct.MakeIDCT(&dctMatrix, &img.Pix, x, y, bounds.Max.X, 4, int8(channelSelected))
 			currentSymbol++
 		}
+	}
+	return true
+}
 
+func Decode(img *image.RGBA, pass string, encodedWordLen int, channelSelected int) bool {
+	bounds := (*img).Bounds()
+	if bounds.Max.X < 8 || bounds.Max.Y < 8 {
+		return false
+	}
+
+	const countValidIndexes = 25
+	const sizeOfBlock2D = 64
+	const sizeOfBlock1D = 8
+	validIndexes := [countValidIndexes]int{7, 14, 15, 21, 22, 23, 28, 29, 30, 31, 35, 36, 37, 38, 42, 43, 44, 45, 48, 49, 50, 51, 55, 56, 57}
+
+	currentSymbol := 0
+	dctMatrix := make([]float32, sizeOfBlock2D)
+	codedWordCounter := make([]int, encodedWordLen)
+	codedWordBool := make([]bool, encodedWordLen)
+	blocksCount := 0
+
+	for x := 0; x < bounds.Max.X-bounds.Max.X%sizeOfBlock1D-1; x += sizeOfBlock1D {
+		for y := 0; y < bounds.Max.Y-bounds.Max.Y%sizeOfBlock1D-1; y += sizeOfBlock1D {
+			if currentSymbol%encodedWordLen == 0 {
+				blocksCount++
+			}
+
+			pixelIndex1 := validIndexes[pass[y%len(pass)]%countValidIndexes]
+			pixelIndex2 := validIndexes[pass[x%len(pass)]%countValidIndexes]
+			pixelIndex3 := validIndexes[abs(pass[y%len(pass)]%countValidIndexes-pass[x%len(pass)]%countValidIndexes)%countValidIndexes]
+
+			for pixelIndex1 == pixelIndex2 {
+				pixelIndex2++
+				pixelIndex2 = validIndexes[pixelIndex2%countValidIndexes]
+			}
+			for (pixelIndex2 == pixelIndex3) || (pixelIndex1 == pixelIndex3) {
+				pixelIndex3++
+				pixelIndex3 = validIndexes[pixelIndex3%countValidIndexes]
+			}
+
+			dct.MakeDCT(&dctMatrix, &(*img).Pix, x, y, bounds.Max.X, 4, int8(channelSelected))
+			// Encoded '1'
+			if dctMatrix[pixelIndex3] <= dctMatrix[pixelIndex2] && dctMatrix[pixelIndex3] <= dctMatrix[pixelIndex1] {
+				codedWordCounter[currentSymbol%encodedWordLen]++
+			}
+			currentSymbol++
+		}
+	}
+
+	for i := 0; i < encodedWordLen; i++ {
+		if codedWordCounter[i] >= ((blocksCount + 1) / 2) {
+			codedWordBool[i] = true
+		}
 	}
 
 	return true
